0f90385e0a66836bee573ae7dafb92c84e2a24fa
MVPilgrim <mv.pilgrim.aws@gmail.com>
2020-08-05 15:22:57 -0500

    Initial version.

---
+++ vars/DisplayStageBanner.groovy
@@ -0,0 +1,28 @@
+/************************************************************
+/* Module: CreateDevTestRelease.groovy
+/*
+/* Description: Display an "eye-catcher" banner in the log file for
+/*              the pipeline stage being executed.
+/*
+/* Change Log:
+/*   * KPS 08-04-2020: Initial version.
+/**********************************************************/
+
+
+def call(stageName) {
+  String topBottomString = "***************************************************************************************";
+  String middleString    = "*                                                                                     *";
+  String blanks          = "                                                                                       ";
+  
+  stageNameLen = stageName.length;
+  int halfLen  = (stageNameLen / 2) - 1;
+  String subBlanks = blanks.subString(0,halfLen);
+  
+  echo topBottomString;
+  echo middleString;
+  echo middleString;
+  echo "*" + blanks.subBlanks + stageName + subBlanks + "*";
+  echo middleString;
+  echo middleString;
+  echo topBottomString;
+}
