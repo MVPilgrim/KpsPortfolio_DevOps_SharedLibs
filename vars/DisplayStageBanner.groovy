d99ed195e6b2410e72ac13fb8dfc4eead43fa739
MVPilgrim <mv.pilgrim.aws@gmail.com>
2020-08-05 17:22:21 -0500

    Debug.

--- vars/DisplayStageBanner.groovy
+++ vars/DisplayStageBanner.groovy
@@ -15,13 +15,14 @@ def call(stageName) {
   blanks          = "                                                                                       ";
   
   stageNameLen = stageName.length();
-  halfLen  = ((stageNameLen / 2) - 1) as Integer;
-  subBlanks = blanks.substring(0,halfLen);
+  blanksLen    = blanks.length();
+  padLen       = ((blanksLen - stageNameLen) / 2) as Integer;
+  padBlanks    = blanks.substring(0,padLen);
   
   echo topBottomString;
   echo middleString;
   echo middleString;
-  echo "*" + subBlanks + stageName + subBlanks + "*";
+  echo "*" + padBlanks + stageName + padBlanks + "*";
   echo middleString;
   echo middleString;
   echo topBottomString;
